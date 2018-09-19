c5d67b0586d9e1dfe89d5bf228c275087f4f1699
Jamie Brynes <jamiebrynes@improbable.io>
2018-09-19 16:11:52 +0100

    init commit

---
+++ Packages/BrokenPackage/BrokenCode.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrokenCode 
+{
+    public BrokenCode()
+    {
+        var thisWillNeverCompile = new WhatIsThisThing();
+    }
+}
---
+++ Packages/WorkingPackage/EditorMenus.cs
@@ -0,0 +1,11 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public static class EditorMenus
+{
+    [MenuItem("Test menu/Do the thing")]
+    public static void LogThing()
+    {
+        Debug.Log("pressed the button");
+    }
+}
